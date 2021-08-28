294b9a17ea54f2b060646de673c4c756745b1594
Natã Romão Ferreira <nataromao@pop-os.localdomain>
2021-08-28 12:37:35 -0300

    Adiciona rotas

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoticiasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('teste', function () {
-    return 'Testando';
+Route::prefix('noticias')->group(function () {
+    Route::get('', [NoticiasController::class, 'index']);
+    Route::post('/create', [NoticiasController::class, 'store']);
 });
