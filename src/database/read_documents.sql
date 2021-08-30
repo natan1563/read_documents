37c70d499bc78374293d31fdfd5c650b5f83ddc2
Natã Romão Ferreira <nataromao@pop-os.localdomain>
2021-08-29 22:23:58 -0300

    Base de dados

---
+++ src/database/read_documents.sql
@@ -0,0 +1,17 @@
+-- --------------------------------------------------------
+-- Servidor:                     localhost
+-- Versão do servidor:           10.3.28-MariaDB-1:10.3.28+maria~focal - mariadb.org binary distribution
+-- OS do Servidor:               debian-linux-gnu
+-- HeidiSQL Versão:              11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para read_documents
+CREATE DATABASE IF NOT EXISTS `read_documents` /*!40100 DEFAULT CHARACTER SET utf16 */;
